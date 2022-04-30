82e2f34f6fe80dfb603b642cd92e17fe3584e1a2
Tony Chen <tonychen7878@gmail.com>
2022-04-30 16:36:04 -0400

    initial files

---
+++ Cornerr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cornerr
+//
+//  Created by Tony Chen on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
